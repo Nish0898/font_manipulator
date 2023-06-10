e21adf63e4f3ddc4c33cee8f3370a23ad449dcfb
Nish0898 <107861901+Nish0898@users.noreply.github.com>
2023-06-10 11:49:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
         canvas = createCanvas(550, 550);
     canvas.position(700,150);
@@ -9,7 +14,11 @@ function setup() {
 }
 function draw() {
     background('#969A97');
-}
+
+    document.getElementById("text_side").innerHTML = "Width and Height of a text will be = " + difference +"px";
+    textSize(difference);
+    fill("#F90093");
+    text("Nishchal", 50, 400);}
 function modelLoaded() {
     console.log('PoseNet Is Initialized!');
 }
@@ -17,6 +26,16 @@ function gotPoses(results)
 {
     if(results.length > 0)
     {
-        console.log(results)
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY =" + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "differnce = " + difference);
+        
     }
 }
